508b750fa589a2a74c15f919e6423388e29c5368
zhdhfhd33 <zhdhfhd33@naver.com>
2022-03-23 22:44:33 +0900

    maek signupController, made interface MemberRepository
    signupController 만들기
    interface MemberRepository생성
    MemeberRepository -> MemberRepositoryImp 변경
    signupService 구현, test코드 작성

---
+++ src/main/java/team3/OneSubscribe/repository/MemberRepositoryImp.java
@@ -0,0 +1,34 @@
+package team3.OneSubscribe.repository;
+
+import org.springframework.stereotype.Repository;
+import team3.OneSubscribe.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepositoryImp implements MemberRepository {
+
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    @Override
+    public int loginCheck(String loginId, String loginPw) {
+        return 0;
+    }
+
+    @Override
+    public Member findByLoginId(String loginId) {
+        return null;
+    }
+}
