04a9a15c25d631d0bbdfda8ef17a4067653bf31a
Amenable-C <duswo0624@naver.com>
2022-05-26 19:55:37 +0900

    Feat : Add update and delete function that are improving

--- src/main/java/team3/OneSubscribe/repository/WritingRepository.java
+++ src/main/java/team3/OneSubscribe/repository/WritingRepository.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import team3.OneSubscribe.domain.Answer;
+import team3.OneSubscribe.domain.Member;
+import team3.OneSubscribe.domain.Tag;
 import team3.OneSubscribe.domain.Writing;
 
 
@@ -33,14 +35,30 @@ public class WritingRepository {
                 .getResultList();
     }
 
+    public List<Writing> findByMember(Member member){
+        return em.createQuery("select i from Writing i where i.member = :member", Writing.class)
+                .setParameter("member", member)
+                .getResultList();
+    }
+
     public List<Writing> findListPaging(int startIndex, int pageSize){
-        return em.createQuery("select b from Writing b", Writing.class)
+        return em.createQuery("select i from Writing i", Writing.class)
                 .setFirstResult(startIndex)
                 .setMaxResults(pageSize)
                 .getResultList();
     }
 
+    public List<Writing> findListPagingForOwn(Member member, int startIndex, int pageSize){
+        return em.createQuery("select i from Writing i where i.member = :member", Writing.class)
+                .setParameter("member", member)
+                .setFirstResult(startIndex)
+                .setMaxResults(pageSize)
+                .getResultList();
+    }
 
-
-
+    //삭제 기능 // 테스트 해야 함
+    public void deleteOne(Long id){
+        em.createQuery("delete from Writing i where i.id = : id", Writing.class)
+                .setParameter("id", id);
+    }
 }
