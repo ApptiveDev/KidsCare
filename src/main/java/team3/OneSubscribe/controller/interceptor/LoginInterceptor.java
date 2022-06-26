9ae5fa72c713062b99aff6d466d7523d7a383bc2
zhdhfhd33 <zhdhfhd33@naver.com>
2022-06-26 15:08:08 +0900

    update
    update

--- src/main/java/team3/OneSubscribe/controller/interceptor/LoginInterceptor.java
+++ src/main/java/team3/OneSubscribe/controller/interceptor/LoginInterceptor.java
@@ -1,6 +1,7 @@
 package team3.OneSubscribe.controller.interceptor;
 
 import lombok.NoArgsConstructor;
+import org.hibernate.Session;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import team3.OneSubscribe.domain.Member;
@@ -26,22 +27,39 @@ public class LoginInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Member m = (Member) request.getSession(false).getAttribute("member");//세션 없을 때 세션 생성 x
+        HttpSession sess = request.getSession(false);
+        boolean flag;
+        if (sess != null) {
+            Member m = (Member) sess.getAttribute("member");//세션 없을 때 세션 생성 x
+
+
 //        HttpSession m =  request.getSession(false);//세션의 유무만을 확인하니까 통과해버린다.
 //        Enumeration<String> em = m.getAttributeNames();
 //        System.out.println("모든 sesseion attri key");
 //        while (em.hasMoreElements()) {
 //            System.out.println(em.nextElement());
 //        }
-        if (m != null) {
-            return true;//접근허용
+            if (m != null) {
+                flag=true;//접근허용
+
+            }
+            else{
+                flag= false;
+            }
         } else {
+
+            flag= false;
+        }
+        if (!flag) {
             String destUri = request.getRequestURI();
             String destQuery = request.getQueryString();
             String dest = (destQuery == null) ? destUri : destUri + "?" + destQuery;
 //            request.getSession().setAttribute("dest", dest); //굳이 세션에 등록할 필요가?
             response.sendRedirect("/member/login");
-            return false;
+            return flag;
+        }
+        else{
+            return flag;
         }
     }
 }
